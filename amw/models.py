4410d009512a5576adc72fc23169836dd8b05eb6
DirtyCajunRice <nick@cajun.pro>
2018-04-19 23:20:40 -0500

    made some changes

--- amw/models.py
+++ amw/models.py
@@ -5,6 +5,7 @@ class TVShow(models.Model):
     silenced = models.BooleanField()
     continuing = models.BooleanField()
     tvdbid = models.CharField(max_length=20, default='')
+    bannerurl = models.CharField(max_length=20, default='')
 
 class ServerInfo(models.Model):
     url = models.CharField(max_length=200)
